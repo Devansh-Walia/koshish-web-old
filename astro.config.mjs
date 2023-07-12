158cb2c5133fbbcb668646a246759e81f15418ce
devansh <devanshwalia9898@gmail.com>
2023-07-12 23:18:32 +0530

    astro init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
+import react from "@astrojs/react"
+
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	outDir: "./build",
+	site: "https://koshishfamily.org",
+	integrations: [
+		react(),
+		sitemap({
+			changefreq: "weekly",
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+		tailwind(),
+	],
+})
