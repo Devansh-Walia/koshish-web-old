654013f0231be2a9cf17c32352ef2d4828cb73b8
devansh <devanshwalia9898@gmail.com>
2023-07-22 21:55:14 +0530

    adds footer

---
+++ src/data/footer.ts
@@ -0,0 +1,17 @@
+export const socialLinks = [
+    // {
+    //     title: 'Twitter',
+    //     icon: '/images/footer/twitter.svg',
+    //     href: 'https://twitter.com/koshishFamily',
+    // },
+    {
+        title: 'Youtube',
+        icon: '/images/footer/youtube.svg',
+        href: 'https://www.youtube.com/channel/UC-tJISVz2rmDxJ-XHKWOx7w?app=desktop',
+    },
+    {
+        title: 'Instagram',
+        icon: '/images/footer/instagram.svg',
+        href: 'https://www.instagram.com/koshish_family/',
+    },
+]
