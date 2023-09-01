7b463f40ff5cf9e46c445fef5c4419fd07cd7d69
Devansh-Walia <devanshwalia9898@gmail.com>
2023-09-01 23:47:31 +0530

    basic timeline update

--- src/components/time-line/data.ts
+++ src/components/time-line/data.ts
@@ -1,51 +1,63 @@
-export const data = [
-    {
-      year: 2012,
-      details: "Journey started with teaching One kid. Raised Funds for him by selling handmade cards outside College canteen. Gave our efforts a name 'Koshish'"
-    },
-    {
-      year: 2013,
-      details: "Created a Vision of providing education for the underprivileged kids, added more kids and members to Koshish and made it a family."
-    },
-    {
-      year: 2014,
-      details: "Started Sending all kids to school taking care of their basic necessities and started celebrating major festivals likes Holi, Diwali, Rakhi, etc."
-    },
-    {
-      year: 2015,
-      details: "Became a fully organized team, recruiting members from all over the city running campaigns like Installation of Dustbins, Distribution Drives and Craft Events."
-    },
-    {
-      year: 2016,
-      details: "Took one step ahead in taking responsibilities of our kids' health, hygiene, and sanitation. Started organizing Health and Dental Camps for Kids and members."
-    },
-    {
-      year: 2017,
-      details: "Visited Orphanage for the first time. Started organizing fruits and clothes distribution drives"
-    },
-    {
-      year: 2018,
-      details: "First batch of Kids completed their board exams. Planned Kids Annual Trips to Museums, Zoos, etc"
-    },
-    {
-      year: 2019,
-      details: "Collaboration with other NGOs and societies to help the needy and grow in the process"
-    },
-    {
-      year: 2020,
-      details: "First Kid sent for Graduation fully sponsored by Koshish Family. Organized Digital classes during Covid Pandemic for all our kids."
-    },
-    {
-      year: 2021,
-      details: "Awareness Sessions, Summer Camps, Art n Craft Competitions, Yoga Sessions started on a regular basis."
-    },
-    {
-      year: 2022,
-      details: 'Koshish Family Completed 10 years. "Aashiyana" a place to study and grow with full amenities was created for Koshish Kids'
-    },
-    {
-      year: 2023,
-      details: 'Koshish Family is registered with high hopes of improving lives with the mission to get formal education for all underprivileged Koshish Kids'
-    }
-  ];
-  
+export interface TimeLineDataType {
+	year: number
+	details: string
+}
+
+export const TimeLineData = [
+	{
+		year: 2012,
+		details:
+			"Journey started with teaching One kid. Raised Funds for him by selling handmade cards outside College canteen. Gave our efforts a name 'Koshish'",
+	},
+	{
+		year: 2013,
+		details:
+			"Created a Vision of providing education for the underprivileged kids, added more kids and members to Koshish and made it a family.",
+	},
+	{
+		year: 2014,
+		details:
+			"Started Sending all kids to school taking care of their basic necessities and started celebrating major festivals likes Holi, Diwali, Rakhi, etc.",
+	},
+	{
+		year: 2015,
+		details:
+			"Became a fully organized team, recruiting members from all over the city running campaigns like Installation of Dustbins, Distribution Drives and Craft Events.",
+	},
+	{
+		year: 2016,
+		details:
+			"Took one step ahead in taking responsibilities of our kids' health, hygiene, and sanitation. Started organizing Health and Dental Camps for Kids and members.",
+	},
+	{
+		year: 2017,
+		details: "Visited Orphanage for the first time. Started organizing fruits and clothes distribution drives",
+	},
+	{
+		year: 2018,
+		details: "First batch of Kids completed their board exams. Planned Kids Annual Trips to Museums, Zoos, etc",
+	},
+	{
+		year: 2019,
+		details: "Collaboration with other NGOs and societies to help the needy and grow in the process",
+	},
+	{
+		year: 2020,
+		details:
+			"First Kid sent for Graduation fully sponsored by Koshish Family. Organized Digital classes during Covid Pandemic for all our kids.",
+	},
+	{
+		year: 2021,
+		details: "Awareness Sessions, Summer Camps, Art n Craft Competitions, Yoga Sessions started on a regular basis.",
+	},
+	{
+		year: 2022,
+		details:
+			'Koshish Family Completed 10 years. "Aashiyana" a place to study and grow with full amenities was created for Koshish Kids',
+	},
+	{
+		year: 2023,
+		details:
+			"Koshish Family is registered with high hopes of improving lives with the mission to get formal education for all underprivileged Koshish Kids",
+	},
+]
